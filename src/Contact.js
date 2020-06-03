a570e0194df32fc247a23ec9fd07a6767e56d8da
Łukasz <lukasz.ulenberg@onet.pl>
2020-06-03 02:09:55 +0200

    poczatek

---
+++ src/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Contact = () => (
+  <div>
+    <h2>Lorem ipsum</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan sapien leo, ac consectetur orci faucibus ac. Ut id est nec nisl bibendum luctus. Nulla eget est at eros mattis pellentesque. Sed in scelerisque lorem, quis commodo velit. Aenean blandit id velit a cursus. Donec gravida imperdiet nisl vitae sodales. Sed urna arcu, rutrum vitae placerat interdum, tincidunt a magna.
+
+In accumsan, lectus eget commodo consectetur, libero est semper mi, quis sagittis nulla arcu vel tortor. Donec dignissim aliquet lacus ut pharetra. Nullam non libero arcu. Morbi lectus elit, auctor sed efficitur non, consectetur et diam. Nulla tincidunt porta ipsum, sed eleifend mi finibus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sollicitudin diam quis posuere suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec euismod facilisis felis, at lacinia urna. Phasellus id ultrices ligula. Vestibulum pharetra vestibulum orci, ac facilisis erat rutrum at. Pellentesque efficitur maximus posuere. Maecenas non dui quis purus laoreet viverra. Donec nibh elit, gravida eget efficitur in, pellentesque nec justo. Duis efficitur id nunc ut venenatis. Etiam eu mi porttitor, vulputate orci at, convallis odio.</p>
+  </div>
+)
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Row, Col, Grid } from 'react-bootstrap';
+
+export const Home = () => (
+  <div>
+    <h2>Sprzęty</h2>
+      <p>Ogarnijcie jak to ma być ma sie dodawac przez formularz czy jak i w jaki sposob chcecie włączać i wyłączać sprzęty oraz co z resztą</p>
+  </div>
+)
